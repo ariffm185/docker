1edfd92bb997125c38ce8d6ca2d421a7590d4644
ariffm185 <ariffm185@gmail.com>
2025-04-03 19:19:06 +0530

    feat:add new file

---
+++ Controller.java
@@ -0,0 +1,15 @@
+
+public Class RestContrl{
+
+    int a;
+    String b;
+
+    RestContrl(int a,String b){
+       this.a=a;
+       this.b=b;
+    }
+}
+public static void main(String[] args){
+    RestContrl r= new RestContrl(10,"Sachin");
+    //changes made
+}
